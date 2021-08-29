1b0b4d393c63c1c8f35cc0ea9f9aca304afb4ab3
sexroute <zhaodali78@gmail.com>
2021-08-29 17:50:04 -0300

    lfs

---
+++ AntiCloudFare/MainWindow.Designer.cs
@@ -0,0 +1,113 @@
+﻿
+namespace AntiCloudFare
+{
+    partial class MainWindow
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.commandToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.startToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.stopToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.webView21 = new Microsoft.Web.WebView2.WinForms.WebView2();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.webView21)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.commandToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1254, 25);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // commandToolStripMenuItem
+            // 
+            this.commandToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.startToolStripMenuItem,
+            this.stopToolStripMenuItem});
+            this.commandToolStripMenuItem.Name = "commandToolStripMenuItem";
+            this.commandToolStripMenuItem.Size = new System.Drawing.Size(80, 21);
+            this.commandToolStripMenuItem.Text = "&Command";
+            // 
+            // startToolStripMenuItem
+            // 
+            this.startToolStripMenuItem.Name = "startToolStripMenuItem";
+            this.startToolStripMenuItem.Size = new System.Drawing.Size(103, 22);
+            this.startToolStripMenuItem.Text = "&Start";
+            this.startToolStripMenuItem.Click += new System.EventHandler(this.startToolStripMenuItem_Click);
+            // 
+            // stopToolStripMenuItem
+            // 
+            this.stopToolStripMenuItem.Name = "stopToolStripMenuItem";
+            this.stopToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.stopToolStripMenuItem.Text = "St&op";
+            this.stopToolStripMenuItem.Click += new System.EventHandler(this.stopToolStripMenuItem_Click);
+            // 
+            // webView21
+            // 
+            this.webView21.CreationProperties = null;
+            this.webView21.DefaultBackgroundColor = System.Drawing.Color.White;
+            this.webView21.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.webView21.Location = new System.Drawing.Point(0, 25);
+            this.webView21.Name = "webView21";
+            this.webView21.Size = new System.Drawing.Size(1254, 653);
+            this.webView21.TabIndex = 1;
+            this.webView21.ZoomFactor = 1D;
+            // 
+            // MainWindow
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1254, 678);
+            this.Controls.Add(this.webView21);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "MainWindow";
+            this.Text = "MainWindow";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.webView21)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem commandToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem startToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem stopToolStripMenuItem;
+        private Microsoft.Web.WebView2.WinForms.WebView2 webView21;
+    }
+}
+
---
+++ AntiCloudFare/MainWindow.cs
@@ -0,0 +1,238 @@
+﻿using CefSharp;
+using CefSharp.WinForms;
+using EricZhao.UiThread;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AntiCloudFare
+{
+    public partial class MainWindow : Form
+    {
+
+        public MainWindow()
+        {
+            this.loadSettings();
+            InitializeComponent();
+            InitializeAsync();
+            this.webView21.NavigationCompleted += WebView21_NavigationCompleted;
+
+        }
+
+        IniFile lpIniFile = new IniFile("./settings.ini");
+        String Url = "http://ai.bhxz.net:1988/floor_price/?";
+        public void loadSettings()
+        {
+            Url = lpIniFile.IniReadStringValue("server", "url", "http://ai.bhxz.net:1988/floor_price/?", true);
+
+        }
+        async private void WebView21_NavigationCompleted(object sender, Microsoft.Web.WebView2.Core.CoreWebView2NavigationCompletedEventArgs e)
+        {
+            //this.webView21.CoreWebView2.ExecuteScriptAsync($"alert('test')')");
+            /* string res = await webView21.ExecuteScriptAsync("document.getElementsByClassName(\"Overflowreact__OverflowContainer-sc-10mm0lu-0 fqMVjm Price--amount\")");
+             MessageBox.Show(res);
+            String lstrHTML = await webView21.ExecuteScriptAsync("document.documentElement.outerHTML");*/
+            String lstrHTML = await webView21.ExecuteScriptAsync("document.documentElement.outerHTML");
+            parseContect(lstrHTML);
+        }
+
+        void parseContect(String astrDoc)
+        {
+            try
+            {
+                string html = astrDoc;
+                html = Regex.Unescape(html);
+                String lstrKey = "Overflowreact__OverflowContainer-sc-10mm0lu-0 fqMVjm Price--amount";
+                int lnIndex = html.IndexOf(lstrKey);
+                if (lnIndex > 0)
+                {
+                    int lnNewStart = lstrKey.Length + lnIndex + 2;
+                    int lnNewEnd = html.IndexOf("<span", lnNewStart + 2);
+                    String lstrValue = html.Substring(lnNewStart, lnNewEnd - lnNewStart);
+                    double ldblValue = 0;
+                    if(Double.TryParse(lstrValue,out ldblValue))
+                    {
+                        lpTaskResults.Add(strCurrentTaskSlug, ldblValue);
+                    }
+                   // MessageBox.Show(lstrValue + "");
+                }
+            }
+            catch (Exception e)
+            {
+                ThreadUiController.log(e.Message);
+            }
+            TaskRunning = 0;
+        }
+
+        async void InitializeAsync()
+        {
+            await webView21.EnsureCoreWebView2Async(null);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            InitializeAsync();
+            this.startUpdater();
+        }
+
+        private void InvokeUI(Action a)
+        {
+            this.BeginInvoke(new MethodInvoker(a));
+        }
+
+        int taskRunning = 0;
+        String strCurrentTaskSlug = "";
+        public System.String StrCurrentTaskSlug
+        {
+            get { lock (this) return strCurrentTaskSlug; }
+            set { lock (this) strCurrentTaskSlug = value; }
+        }
+        Dictionary<String, Double> lpTaskResults = new Dictionary<string, double>();
+        public int TaskRunning
+        {
+            get { lock (this) return taskRunning; }
+            set { lock (this) taskRunning = value; }
+        }
+        async private void ThreadEntry()
+        {
+           while(true)
+            {
+                try
+                {
+                    this.ThreadTask();
+                    Thread.Sleep(1*60 * 1000);
+                }catch(Exception e)
+                {
+                    ThreadUiController.log(e.Message);
+                }
+            }
+        }
+
+        async private void ThreadTask()
+        {
+            try
+            {
+                //1.get task
+                List<String> lpTaskSlugnames = new List<string>();
+                using (var client = new WebClient())
+                {
+                    String lstrData = client.DownloadString(Url + "query");
+                    if (null != lstrData)
+                    {
+                        String[] lpSplits = lstrData.Split(';');
+                        for (int i = 0; i < lpSplits.Length; i++)
+                        {
+                            String lstrTask = lpSplits[i];
+                            if (!String.IsNullOrWhiteSpace(lstrTask))
+                            {
+                                lpTaskSlugnames.Add(lstrTask);
+                            }
+                        }
+                    }
+                }
+
+                //1.1 clear result
+                lpTaskResults.Clear();
+
+                //2.exec task and wait for task finished
+                foreach (String lpTask in lpTaskSlugnames)
+                {
+                    TaskRunning = 1;
+                    InvokeUI(() =>
+                    {
+                        StrCurrentTaskSlug = lpTask;
+                        doLoad(lpTask);
+                    });
+
+                    while (true)
+                    {
+                        int lnValue = 0;
+                        lock (this)
+                        {
+                            lnValue = TaskRunning;
+                        }
+                        if (lnValue <= 0)
+                        {
+                            break;
+                        }
+                        
+                        {
+                            Thread.Sleep(3000);
+                        }
+                    }
+                }
+
+                //3.post response
+                using (var client = new WebClient())
+                {
+                    StringBuilder loSb = new StringBuilder();
+
+                    foreach (KeyValuePair<String, Double> lpResult in this.lpTaskResults)
+                    {
+                        loSb.Append(lpResult.Key + ":" + lpResult.Value);
+                        loSb.Append(";");
+                    }
+
+                    String lstrData = client.DownloadString(Url + "set=" + loSb.ToString());
+
+                }
+
+            }
+            catch (Exception e)
+            {
+
+            }
+        }
+
+
+        private void doLoad(String lstrSlugName)
+        {
+            this.webView21.CoreWebView2.Navigate(String.Format("https://opensea.io/collection/{0}?search[sortAscending]=true&search[sortBy]=PRICE&search[toggles][0]=BUY_NOW", lstrSlugName));
+        }
+
+        Thread lpThread = null;
+        public System.Threading.Thread LpThread
+        {
+            get { lock(this)return lpThread; }
+            set { lock (this) lpThread = value; }
+        }
+        private void startUpdater()
+        {
+            if (LpThread == null)
+            {
+                LpThread = new Thread(this.ThreadEntry);
+                LpThread.Start();
+            }
+        }
+        private void startToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.startUpdater();
+        }
+
+        async private void stopToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (LpThread != null)
+            {
+                try
+                {
+                    LpThread.Abort();
+
+                }
+                catch (Exception e1)
+                {
+
+                }
+            }
+            LpThread = null;
+        }
+    }
+}
